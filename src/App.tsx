d1f217e4ca7b2cbcbb01116f6ec5a0f4088d7313
Tony Martinez <ajosephmartinez@gmail.com>
2020-10-27 22:55:15 -0700

    Urql + auth0 + hasura flow working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState, useEffect } from "react";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { createClient, Provider } from "urql";
+import Todos from "./components/Todos";
 
-function App() {
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
   );
-}
+};
+
+const App = () => {
+  const [show, setShow] = useState(false);
+  const [token, setToken] = useState("");
+
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    getAccessTokenSilently,
+    getIdTokenClaims,
+  } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const getToken = async () => {
+        // const accessToken = await getAccessTokenSilently({
+        //   audience: `https://the-hackboard.us.auth0.com/api/v2/`,
+        //   scope: "read:current_user",
+        // });
+        const accessToken = await getAccessTokenSilently();
+        console.log("token", accessToken);
+        setToken(accessToken);
+      };
+
+      getToken();
+    }
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  const client = createClient({
+    url: "https://the-hackboard.herokuapp.com/v1/graphql",
+    fetchOptions: () => {
+      // const token = getToken();
+      return {
+        headers: { authorization: token ? `Bearer ${token}` : "" },
+      };
+    },
+  });
+
+  return (
+    <Provider value={client}>
+      <div>
+        <LoginButton />
+        <LogoutButton />
+        <button onClick={() => setShow(!show)}>click</button>
+        {show && <Todos />}
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useQuery } from "urql";
+const TodosQuery = `
+  query {
+    posts {
+      title
+    }
+  }
+`;
+
+const Todos = () => {
+  const [result, reexecuteQuery] = useQuery({
+    query: TodosQuery,
+  });
+  console.log("result?", result);
+  const { data, fetching, error } = result;
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default Todos;
