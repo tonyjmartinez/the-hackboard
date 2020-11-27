28a3b88f5b5831cc066e0bfa251b43452a1135df
Tony Martinez <ajosephmartinez@gmail.com>
2020-11-26 21:37:09 -0800

    .

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({ maximumFileSizeToCacheInBytes: 4000000 });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
