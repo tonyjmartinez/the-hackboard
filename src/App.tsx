77524a1473abdb5baea1940f8e168f98bf886077
Tony Martinez <ajosephmartinez@gmail.com>
2020-12-14 22:53:21 -0800

    Fix auth error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import "./App.css";
-import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { createClient, Provider } from "urql";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/theme";
@@ -40,30 +40,20 @@ const App = () => {
   });
 
   return (
-    <React.StrictMode>
-      <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
-        redirectUri={window.location.origin}
-        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-        scope={process.env.REACT_APP_AUTH0_SCOPE}
-      >
-        <Provider value={client}>
-          <ChakraProvider theme={theme}>
-            <Router>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Switch>
-                  <Route component={NewPost} path="/new" />
-                  <Route path="/posts/:id" children={<Post />} />
-                  <Route component={Page} path="/" />
-                </Switch>
-                <Nav />
-              </Suspense>
-            </Router>
-          </ChakraProvider>
-        </Provider>
-      </Auth0Provider>
-    </React.StrictMode>
+    <Provider value={client}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route component={NewPost} path="/new" />
+              <Route path="/posts/:id" children={<Post />} />
+              <Route component={Page} path="/" />
+            </Switch>
+            <Nav />
+          </Suspense>
+        </Router>
+      </ChakraProvider>
+    </Provider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,28 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const Application = () => (
+  <React.StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
+      redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      scope={process.env.REACT_APP_AUTH0_SCOPE}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
 
 const rootElement = document.getElementById("root");
+
 if (rootElement?.hasChildNodes()) {
-  hydrate(<App />, rootElement);
+  hydrate(<Application />, rootElement);
 } else {
-  render(<App />, rootElement);
+  render(<Application />, rootElement);
 }
 
 serviceWorkerRegistration.register();
