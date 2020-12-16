6a7bf6f32b9a979883c69ca648dac3a405a9fbaa
Tony Martinez <ajosephmartinez@gmail.com>
2020-12-15 23:35:22 -0800

    .

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ if (rootElement?.hasChildNodes()) {
   render(<Application />, rootElement);
 }
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
