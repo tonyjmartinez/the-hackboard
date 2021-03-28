54bdd40ec87b7719a1d30d05825a21242072fa7f
Tony Martinez <ajosephmartinez@gmail.com>
2021-03-28 12:54:55 -0700

    Change nav to conflict less with mobile/desktop

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from '@auth0/auth0-react'
 import {
   Flex,
   Heading,
@@ -12,71 +12,76 @@ import {
   useColorModeValue,
   Tooltip,
   BoxProps,
-} from "@chakra-ui/react";
-import React from "react";
-import { FiSun, FiMoon, FiPlusCircle } from "react-icons/fi";
-import { Link } from "react-router-dom";
+} from '@chakra-ui/react'
+import React from 'react'
+import {FiSun, FiMoon, FiPlusCircle} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 
-const Nav = ({ ...rest }: BoxProps) => {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isLight = colorMode === "light";
+const Nav = ({...rest}: BoxProps) => {
+  const {loginWithRedirect, isAuthenticated, logout} = useAuth0()
+  const {colorMode, toggleColorMode} = useColorMode()
+  const isLight = colorMode === 'light'
 
-  const theme = useTheme();
+  const theme = useTheme()
 
-  const buttonIconColor = useColorModeValue("cyan", "teal");
-  const iconColor = useColorModeValue("white", theme.colors.blue[800]);
+  const buttonIconColor = useColorModeValue('cyan', 'teal')
+  const iconColor = useColorModeValue('white', theme.colors.blue[800])
 
   return (
-    <Flex w="100%" pos="fixed" {...rest}>
-      <Center bg="blue.600">
-        <Heading size="md" p={3} color="white">
-          <Link to="/">The Hackboard</Link>
-        </Heading>
-      </Center>
-      <Spacer />
-      <Box p="1" bg="blue.600">
-        {isAuthenticated ? (
-          <>
-            <Link to="/new">
-              <Tooltip label="New Post">
-                <IconButton
-                  colorScheme={buttonIconColor}
-                  aria-label="new post"
-                  variant="solid"
-                  icon={<FiPlusCircle size={30} color={iconColor} />}
-                />
-              </Tooltip>
-            </Link>
+    <>
+      <Flex w="100%" pos="fixed" {...rest}>
+        <Box bg="gray.600" opacity="0.8">
+          <Heading size="md" p={3} color="white">
+            <Link to="/">The Hackboard</Link>
+          </Heading>
+        </Box>
+      </Flex>
+      <Flex w="100%" pos="fixed" bottom="0" {...rest}>
+        <Spacer />
+        <Box px={[2, 2, 6]} bg="gray.600" opacity="0.8">
+          {isAuthenticated ? (
+            <>
+              <Link to="/new">
+                <Tooltip label="New Post">
+                  <IconButton
+                    colorScheme={buttonIconColor}
+                    aria-label="new post"
+                    variant="solid"
+                    icon={<FiPlusCircle size={30} color={iconColor} />}
+                  />
+                </Tooltip>
+              </Link>
 
-            <Button
-              m={3}
-              onClick={() => logout({ returnTo: window.location.origin })}
-            >
-              Logout
+              <Button
+                m={3}
+                onClick={() => logout({returnTo: window.location.origin})}
+                colorScheme="teal"
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Button m={3} onClick={loginWithRedirect} colorScheme="teal">
+              Login
             </Button>
-          </>
-        ) : (
-          <Button m={3} onClick={loginWithRedirect}>
-            Login
-          </Button>
-        )}
+          )}
 
-        <IconButton
-          onClick={toggleColorMode}
-          colorScheme={buttonIconColor}
-          aria-label="Dark Mode toggle"
-          icon={
-            isLight ? (
-              <FiMoon size={30} color={iconColor} />
-            ) : (
-              <FiSun size={30} color={iconColor} />
-            )
-          }
-        />
-      </Box>
-    </Flex>
-  );
-};
+          <IconButton
+            onClick={toggleColorMode}
+            colorScheme={buttonIconColor}
+            aria-label="Dark Mode toggle"
+            icon={
+              isLight ? (
+                <FiMoon size={30} color={iconColor} />
+              ) : (
+                <FiSun size={30} color={iconColor} />
+              )
+            }
+          />
+        </Box>
+      </Flex>
+    </>
+  )
+}
 
-export default Nav;
+export default Nav
